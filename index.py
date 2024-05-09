e49b53bbdb424ffd716d0e48765627a440ddeae7
XanderL2 <alexanderllave2021@gmail.com>
2024-05-09 21:54:12 +0200

    Frontend

--- index.py
+++ index.py
@@ -1,17 +1,52 @@
 from flask import Flask, render_template, request;
+import requests as req;
+
+
+
 app = Flask(__name__);
 
 
-#CODIGO:
 
 @app.route("/", methods = ['GET', 'POST'])
-def form_user():
+def root():
     
+
+
+
+
 
     return render_template('index.html');
 
 
-        
+@app.route("/login", methods = ['GET', 'POST'])
+def register():
+    
+
+
+
+    return render_template('login.html');
+
+
+
+@app.route("/register", methods = ['GET', 'POST'])
+def login():
+    
+
+
+
+    return render_template('register.html');
+
+
+@app.route("/index", methods = ['GET', 'POST'])
+def index():
+    
+
+
+
+    return render_template('index.html');
+
+
+       
 
 
 #Server running on:
