520a5bef98f3e6f5277acff05114bcf7f2668996
XanderL2 <alexanderllave2021@gmail.com>
2024-05-11 14:25:50 +0200

    Autencicacion

--- controllers/authentication.py
+++ controllers/authentication.py
@@ -6,22 +6,37 @@ from configs import HOST, API_PORT;
 endpoint =  f'http://{HOST}:{API_PORT}/api/';
 
 
-def AuthUser(username, password): 
+def Authentication(requestForm): 
+
+    username = requestForm.get("username");
+    password = requestForm.get("password");
 
     data = {
-  
-        "username": "UsuarioPrueba",
-        "password": "prueba123"
-  
+        "username": username,
+        "password": password
     };
 
 
-    endpointLogin = endpoint + "login";
+    print(data)
 
-    response = req.post(endpointLogin, data=data);
+    try:
+
+        response = req.post(endpoint + "/login", json=data);
+        return response
+
+    except Exception as e: 
+
+        print(e);
+        return False;
+
+    
+    
+    
 
 
-    return response
+
+
+
     
 
 
--- index.py
+++ index.py
@@ -1,45 +1,101 @@
-from flask import Flask, render_template, request;
+from flask import Flask, render_template, request, make_response, jsonify, redirect;
 import requests as req;
 
 
 import configs;
-import controllers.authentication as auth;
+from controllers.authentication import Authentication
+
 from configs import HOST, API_PORT
 
+
+
+
 endpoint =  f'http://{HOST}:{API_PORT}/api/';
-
-
-
-
 app = Flask(__name__);
 
 
 
+
+
+
+#Index Route
 @app.route("/", methods = ['GET', 'POST'])
 def root():
-    
-
-    response = auth.AuthUser("password", "xd");
-
-
-
     return render_template('index.html', endpoint=endpoint + "/users");
 
 
 
-@app.route("/login", methods = ['GET', 'POST'])
-def register():
-    
 
 
+#Login Routes
 
+@app.route("/login", methods = ['GET'])
+def GETLogin():
     return render_template('login.html');
+
+
+
+@app.route("/login", methods = ['POST'])
+def POSTLogin():
+
+
+    response = Authentication(request.form)
+    body = response.json();
+
+
+    if(response.status_code != 202):
+        return render_template('login.html', error = body.get("Message"));
+
+
+
+    token = body.get("x-access-token");
+
+
+    resp = make_response(render_template("login.html"))
+    resp.set_cookie('token', token)
+
+    return resp;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 
 @app.route("/register", methods = ['GET', 'POST'])
-def login():
+def register():
     
 
 
